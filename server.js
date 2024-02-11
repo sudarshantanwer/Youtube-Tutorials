1ca56828912dc63dd12d7ea33b83e35807434acd
Sudarshan Tanwar <sudarshan@tothenew.com>
2024-02-11 17:30:50 +0530

    htmx with express server

--- server.js
+++ server.js
@@ -3,10 +3,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/home', (req, res)=>{
 
-    res.send('hello');
+let counter = 0;
+app.use(express.static('public'));
+
+app.get('/api/increment', (req, res)=>{
+    counter++;
+    const updatedHTML = `<h2 id="counter">Counter: <span>${counter}</span></h2>`;
+
+    res.send(updatedHTML);
 })
+
+
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
